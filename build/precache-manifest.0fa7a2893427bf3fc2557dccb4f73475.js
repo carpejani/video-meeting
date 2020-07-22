58b5f4777222c9d613f54999f2d4399031c4ff15
Lucas <lucasf854@gmail.com>
2020-07-22 13:55:27 -0300

    rm ignore build

---
+++ build/precache-manifest.0fa7a2893427bf3fc2557dccb4f73475.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88d84a4d1bcd7f7359844bf5bfb40de6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f061fa05aa146d0ca196",
+    "url": "/static/css/2.e0e21483.chunk.css"
+  },
+  {
+    "revision": "34abbd5a3ba532467fb7",
+    "url": "/static/css/main.7e169c8b.chunk.css"
+  },
+  {
+    "revision": "f061fa05aa146d0ca196",
+    "url": "/static/js/2.fb66f22d.chunk.js"
+  },
+  {
+    "revision": "df958f16b07871c3679510165e97a61a",
+    "url": "/static/js/2.fb66f22d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34abbd5a3ba532467fb7",
+    "url": "/static/js/main.5e622f27.chunk.js"
+  },
+  {
+    "revision": "5ff6df50c1a535018958",
+    "url": "/static/js/runtime-main.df338948.js"
+  }
+]);
